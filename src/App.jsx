d47de1bc62489990385355e66c2e30c8d91ef5b2
eaitkaziev <aitkazieverdan@gmail.com>
2024-12-01 13:39:27 +0100

    added more games to my data List

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
-import "./App.css"; 
+import "./App.css";
 
 const fetchGames = async () => {
   const gamesOfChildhood = [
-    { id: 1, name: 'Plant Zombies 2' },
-    { id: 2, name: 'Assasin Creed 2' },
-    { id: 3, name: 'Need For Speed Most Wanted' },
-    { id: 4, name: 'GTA San Andreas' },
-    { id: 5, name: 'Warcraft:Frozen Throne' },
+      { id: 1, name: 'Plant Zombies 2' },
+      { id: 2, name: 'Assassin Creed 2' },
+      { id: 3, name: 'Need For Speed Most Wanted' },
+      { id: 4, name: 'GTA San Andreas' },
+      { id: 5, name: 'Warcraft: Frozen Throne' },
+      { id: 6, name: 'The Witcher 3: Wild Hunt' },
+      { id: 7, name: 'Red Dead Redemption 2' },
+      { id: 8, name: 'Minecraft' },
+      { id: 9, name: 'Overwatch' },
+      { id: 10, name: 'Call of Duty: Modern Warfare' }
   ];
   return gamesOfChildhood;
 };
@@ -22,7 +27,7 @@ function App() {
   const [searchThing, setSearchThing] = useState("");
   useEffect(() => {
     const loadData = async () => {
-      const games = await fetchGames(); 
+      const games = await fetchGames();
       setData(games);
     };
     loadData();
@@ -30,13 +35,13 @@ function App() {
   const filteredData = filterGames(data, searchThing);
   return (
     <div className='searchComponent'>
-        <input
-          type='text'
-          placeholder='Search game...'
-          value={searchThing}
-          onChange={e => setSearchThing(e.target.value)}
-        />
-       <ul>
+      <input
+        type='text'
+        placeholder='Search game...'
+        value={searchThing}
+        onChange={e => setSearchThing(e.target.value)}
+      />
+      <ul>
         {filteredData.map(game => (
           <li key={game.id}>{game.name}</li>
         ))}

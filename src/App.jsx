121ec337978bbdd111928bd1b36f76d83f5b0548
eaitkaziev <aitkazieverdan@gmail.com>
2024-12-01 12:57:24 +0100

    initialized the data list at the first render using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
 	const [searchThing, setSearchThing] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const gamesOfChildhood = [
+      { id: 1, name: 'Plant Zombies 2' },
+      { id: 2, name: 'Assasin Creed 2' },
+      { id: 3, name: 'Need For Speed Most Wanted' },
+      { id: 4, name: 'GTA San Andreas' },
+      { id: 5, name: 'Warcraft:Frozen Throne' },
+    ];
+    setData(gamesOfChildhood);
+  };
+    fetchData();
+},[]);
+
   return (
     <div></div>
 	);
